4eb5605030d5ee4eaf400e8bcfbde9c5f7b7fc39
KDM85 <102189332+KDM85@users.noreply.github.com>
2023-06-16 13:27:35 -0400

    Create settings.py
    Tailor the experience as needed by editing the "content" call in "MESSAGES".

---
+++ settings.py
@@ -0,0 +1,11 @@
+MESSAGES = [
+    {
+        "role": "system",
+        "content": "You are an expert Python developer.",
+    }
+]
+FONT = ("Arial", 14)
+FONT_BOLD = ("Arial", 14, "bold")
+
+BOT_BG = "aquamarine4"
+USER_BG = "RoyalBlue4"
